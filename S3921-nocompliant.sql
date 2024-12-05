6a7ac3de50b4fd62c8583175902a28f5932d072a
Michal Budzowski <Michal.Budzowski@sonarsource.com>
2024-12-05 10:47:11 +0100

    changes in SQL

--- S3921-nocompliant.sql
+++ S3921-nocompliant.sql
@@ -1,5 +1,25 @@
-declare
-  foo varchar2(4);
-begin
-	  foo := 'abcdeghij;' -- foo cannot be assigned such a big value
-end;
+CREATE OR REPLACE PROCEDURE test_procedure IS
+  foo VARCHAR2(4);
+BEGIN
+  BEGIN
+    foo := 'abcdeghij'; -- foo cannot be assigned such a big value
+  EXCEPTION
+    WHEN VALUE_ERROR THEN
+      DBMS_OUTPUT.PUT_LINE('Error: Value too large for variable "foo".');
+  END;
+END;
+
+CREATE TABLE Persons
+(
+  Id   NUMBER,
+  Name VARCHAR2(4)
+);
+
+INSERT INTO Persons (Id, Name) VALUES (1, 'Alice'); -- Noncompliant, raises ORA-12899
+
+CREATE OR REPLACE PROCEDURE sp1
+IS
+  foo VARCHAR2(2);
+BEGIN
+  SELECT Name INTO foo FROM Persons WHERE Id = 1; -- Noncompliant, may raise ORA-06502
+END;
